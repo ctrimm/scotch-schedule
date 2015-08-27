252e2fd1944875f6b499a01026b1c146a8ca5a77
Cory Trimm <cory.trimm@gmail.com>
2015-08-26 22:10:42 -0400

    three way data binding complete. ng-repeat complete. working app

--- app.js
+++ app.js
@@ -8,35 +8,10 @@ angular.module('scheduleApp', ['firebase'])
   var ref = new Firebase("https://scotch-angular-sched.firebaseio.com/days");
   var fb = $firebase(ref);
 
-  $scope.reset = function(){
-    fb.$set({
-        monday: {
-            name: 'Monday', 
-            slots: {
-                0900: {
-                    time: '9:00am',
-                    booked: false
-                },
-                0110: {
-                    time: '11:00am',
-                    booked: false
-                },
-            }
-        },
-        tuesday: {
-            name: 'Tuesday',
-            slots: {
-                0900: {
-                    time: '9:00am',
-                    booked: false
-                },
-                0110: {
-                    time: '11:00am',
-                    booked: false
-                }
-            }
-        }
-    });
-  }
+  // sync as object
+  var syncObject = fb.$asObject();
+
+  // three way data binding
+  syncObject.$bindTo($scope, 'days');
 
 });
