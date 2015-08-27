c5e66cedbedb4c871c000cf04d54f055f2bcf0cf
Cory Trimm <cory.trimm@gmail.com>
2015-08-26 22:03:00 -0400

    reset button works for populating firebase db

--- app.js
+++ app.js
@@ -5,7 +5,38 @@ angular.module('scheduleApp', ['firebase'])
 .controller('mainController', function($scope, $firebase) {
   
   // our application code will go here
-  var ref = new Firebase("https://scotch-angular-sched/firebaseio.com/days");
+  var ref = new Firebase("https://scotch-angular-sched.firebaseio.com/days");
   var fb = $firebase(ref);
+
+  $scope.reset = function(){
+    fb.$set({
+        monday: {
+            name: 'Monday', 
+            slots: {
+                0900: {
+                    time: '9:00am',
+                    booked: false
+                },
+                0110: {
+                    time: '11:00am',
+                    booked: false
+                },
+            }
+        },
+        tuesday: {
+            name: 'Tuesday',
+            slots: {
+                0900: {
+                    time: '9:00am',
+                    booked: false
+                },
+                0110: {
+                    time: '11:00am',
+                    booked: false
+                }
+            }
+        }
+    });
+  }
 
 });
